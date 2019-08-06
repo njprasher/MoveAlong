50fc67f31f8e057e3c908a33a693b58f94ea50a2
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-08-06 13:26:56 -0400

    Initial commit

---
+++ app/src/main/java/com/codeblack/movealong/MainActivity.java
@@ -0,0 +1,14 @@
+package com.codeblack.movealong;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
