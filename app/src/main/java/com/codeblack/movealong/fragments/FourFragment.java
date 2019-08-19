5dcc58c87745f0e42912351d6ec541d0dfa84525
rajwindersidhu92 <kaurrajwinder203@gmail.com>
2019-08-19 16:43:31 -0400

    [Neeraj] Added Contact us Page

--- app/src/main/java/com/codeblack/movealong/fragments/FourFragment.java
+++ app/src/main/java/com/codeblack/movealong/fragments/FourFragment.java
@@ -1,13 +1,17 @@
 package com.codeblack.movealong.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
+import com.codeblack.movealong.MyContactActivity;
+import com.codeblack.movealong.MyWebView;
 import com.codeblack.movealong.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -27,6 +31,9 @@ public class FourFragment extends Fragment {
     private TextView address;
     private TextView email;
     private TextView phone;
+    private Button about_us;
+    private Button contact_us;
+
     FirebaseAuth mAuth;
     public FourFragment() {
         // Required empty public constructor
@@ -35,6 +42,7 @@ public class FourFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
             dbref.collection("users").addSnapshotListener(new EventListener<QuerySnapshot>() {
@@ -64,6 +72,28 @@ public class FourFragment extends Fragment {
         address = mView.findViewById(R.id.lblAddress);
         email = mView.findViewById(R.id.lblEmail);
         phone = mView.findViewById(R.id.lblPhone);
+        about_us = mView.findViewById(R.id.about_us);
+        contact_us = mView.findViewById(R.id.contact_us);
+
+        about_us.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(view.getContext(), MyWebView.class);
+                view.getContext().startActivity(intent);
+            }
+        });
+        contact_us.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(view.getContext(), MyContactActivity.class);
+                view.getContext().startActivity(intent);
+            }
+        });
+
         return mView;
     }
 }
